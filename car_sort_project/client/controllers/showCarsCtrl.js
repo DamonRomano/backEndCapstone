09e2f0f713804a9ad99e7c762f5501f0daa1af6e
Damon Romano <rdromano@mac.com>
2016-09-22 10:54:21 -0500

    filters for horsepower, torque, and accelleration times working.

--- car_sort_project/client/controllers/showCarsCtrl.js
+++ car_sort_project/client/controllers/showCarsCtrl.js
@@ -7,6 +7,19 @@ angular.module('MotoBene')
         function($scope, $http, RootFactory, $timeout) {
             $scope.title = "I'm the page that shows you the sortable table of cars."
 
+            $scope.hpExpr = function(car) {
+                if (!$scope.hpText) return true;
+                return car.horsepower >= $scope.hpText;
+            }
+            $scope.tqExpr = function(car) {
+                if (!$scope.tqText) return true;
+                return car.torque >= $scope.tqText;
+            }
+            $scope.accelExpr = function(car) {
+                if (!$scope.accelText) return true;
+                return car.zero_to_sixty_acceleration_time <= $scope.accelText;
+            }
+
             RootFactory.getApiRoot()
                 .then(
                     root => {
